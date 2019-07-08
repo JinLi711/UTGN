865260f5aed28cde613315c5369ae9b4006cfa94
Jin Li <jinli7255@gmail.com>
2019-07-08 11:41:45 +0800

    changing around CUDA_VISIBLE_DEVICES

--- UTGN/model/protling.py
+++ UTGN/model/protling.py
@@ -225,7 +225,8 @@ def run_model(args):
 
     # set GPU-related environmental options and config settings
     # ??? does this even do anything? Whats the point of setting the environment?
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu) if args.gpu is not None else ''
+    # os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu) if args.gpu is not None else ''
+    os.environ['CUDA_VISIBLE_DEVICES'] = 0
 
     # derived files and directories
     base_dir = args.base_directory
